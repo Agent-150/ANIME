8320cbe7cedf97d8b0ab325653d9fe74b33b4956
Mithun <76549104+Agent-150@users.noreply.github.com>
2023-02-19 19:45:37 +0530

    Update __main__.py

--- main/__main__.py
+++ main/__main__.py
@@ -4,33 +4,31 @@ from main import app
 from pyrogram import filters, idle
 from pyrogram.types import Message
 from uvloop import install
-from contextlib import closing, suppress
 from main.modules.tg_handler import tg_handler
 
 loop = asyncio.get_event_loop()
 
 @app.on_message(filters.command(["start","help","ping"]))
 async def start(bot, message: Message):
-  return await message.reply_text("⭐️ **Bot Is Online...**\n\n**Updates :** @TechZBots **| Support :** @TechZBots_Support")
+    return await message.reply_text("⭐️ **Bot Is Online...**\n\n**Updates :** @TechZBots **| Support :** @TechZBots_Support")
 
 async def start_bot():
-  print("==================================")
-  print("[INFO]: AutoAnimeBot Started Bot Successfully")
-  print("==========JOIN @TECHZBOTS=========")
+    print("==================================")
+    print("[INFO]: AutoAnimeBot Started Bot Successfully")
+    print("==========JOIN @TECHZBOTS=========")
 
-  print("[INFO]: Adding Parsing Task")
-  asyncio.create_task(auto_parser())
-  asyncio.create_task(tg_handler())
-  
-  await idle()
-  print("[INFO]: BOT STOPPED")
-  await app.stop()  
-  for task in asyncio.all_tasks():
-    task.cancel()
+    print("[INFO]: Adding Parsing Task")
+    asyncio.ensure_future(auto_parser())
+    asyncio.ensure_future(tg_handler())
+
+    await idle()
 
 if __name__ == "__main__":
-  install()
-  with closing(loop):
-    with suppress(asyncio.exceptions.CancelledError):
-      loop.run_until_complete(start_bot())
-      loop.run_until_complete(asyncio.sleep(3.0))
+    install()
+    try:
+        loop.run_until_complete(start_bot())
+    except KeyboardInterrupt:
+        pass
+    finally:
+        loop.run_until_complete(asyncio.gather(*asyncio.all_tasks()))
+        loop.close()
